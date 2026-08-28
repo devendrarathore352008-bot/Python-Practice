d795cceabfd91d1b05d48fc920ca8343d1f2fa6c
devendrarathore352008 <devendrarathore352008@gmail.com>
2026-08-28 20:34:26 +0530

    Adding new program (temrature converter)

---
+++ Assignments/temp_converter.py
@@ -0,0 +1,29 @@
+#    Write A Program To Convert Celsius To Fahrenheit (Date:27/08/2026)
+
+# Taking Celsius As Input From The User
+
+C = float(input("Enter the temperature in celsius: "))
+
+# Converting The Temperature Celsius To Fahrenheit 
+
+F = (C * 9/5) + 32
+
+# Printing the necessary statements
+
+print("-----Celsius To Fahrenheit-----")
+print(C,"\u00B0C is equal to", F,"\u00B0F")
+
+
+
+
+
+
+
+
+
+
+
+
+#It is a method called f string to print various values and function between quotes
+
+# print(f"{C}\u00B0C is equal to {F}\u00B0F")
